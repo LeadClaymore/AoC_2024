8b7810b85d53bf9d8a7137a99f0767ed91fcd5d2
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-03 09:38:39 -0500

    three part 2

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     //println!("{:?}", one::_one_p2());
     //println!("{:?}", two::_two_p1());
     //println!("{:?}", two::_two_p2());
-    println!("{:?}", three::_three_p1());
+    //println!("{:?}", three::_three_p1());
+    println!("{:?}", three::_three_p2());
 }
--- src/three.rs
+++ src/three.rs
@@ -100,3 +100,128 @@ fn find_mul(data: &Vec<char>) -> i32 {
     return ret;
 }
 
+pub fn _three_p2() -> io::Result<()> {
+    let mut data: Vec<char> = Vec::new();
+    let mut answer = 0;
+
+    let reader = io::BufReader::new(File::open("data/three.txt")?);
+
+    for line_res in reader.lines() {
+        let line = line_res?;
+        
+        if line.trim().is_empty() {
+            continue;
+        } else {
+            for ii in line.chars() {
+                data.push(ii);
+            }
+        }
+    }
+    answer = find_mul_p2(&data);
+
+    println!("answer = {}", answer);
+    Ok(())
+}
+
+fn find_mul_p2(data: &Vec<char>) -> i32 {
+    let mut ret: i32 = 0;
+    let len = data.len();
+    let mut index = 0;
+    let mut num_size_1;
+    let mut num_size_2;
+    let mut allow = true;
+    loop {
+        (num_size_1, num_size_2) = (0, 0);
+
+        // enable mul
+        if 
+            len - index > 4 &&
+            data[index] == 'd' &&
+            data[index + 1] == 'o' &&
+            data[index + 2] == '(' &&
+            data[index + 3] == ')' 
+        {
+            allow = true;
+        } 
+        // disable the mul
+        else if 
+            len - index > 7 &&
+            data[index] == 'd' &&
+            data[index + 1] == 'o' &&
+            data[index + 2] == 'n' &&
+            data[index + 3] == '\'' &&
+            data[index + 4] == 't' &&
+            data[index + 5] == '(' &&
+            data[index + 6] == ')' 
+        {
+            allow = false;
+        }
+        if allow {
+            // 8 is the min size of multipliable numbers
+            if 
+                len - index > 8
+            {
+                if
+                    data[index] == 'm' &&
+                    data[index + 1] == 'u' &&
+                    data[index + 2] == 'l' &&
+                    data[index + 3] == '(' 
+                {
+                    while data[index + 4 + num_size_1].is_numeric() {
+                        num_size_1 += 1;
+                        if 
+                            // 7 cause num size should inculde at least 1 that was the min of the command size
+                            len - index > 7 + num_size_1 &&
+                            data[index + 4 + num_size_1].is_numeric() 
+                        {
+                            // if the current one is numeric then continue
+                            continue;
+                        } else {
+                            // if its not then its the last digit
+                            break;
+                        }
+                    }
+                    if 
+                        num_size_1 != 0 &&
+                        data[index + 4 + num_size_1] == ',' 
+                    {
+                        while data[index + 5 + num_size_1 + num_size_2].is_numeric() {
+                            num_size_2 += 1;
+                            if 
+                                len - index > 6 + num_size_1 + num_size_2 &&
+                                data[index + 5 + num_size_1 + num_size_2].is_numeric() 
+                            {
+                                // if the current one is numeric then continue
+                                continue;
+                            } else {
+                                // if its not then its the last digit
+                                break;
+                            }
+                        }
+                        if
+                            num_size_2 != 0 &&
+                            data[index + 5 + num_size_1 + num_size_2] == ')' 
+                        {
+                            // turns num 1 and num 2 into real numbers
+                            let (mut num1, mut num2) = (0, 0);
+                            for ii in (index + 4)..(index + 4 + num_size_1){
+                                num1 = num1 * 10 + data[ii].to_digit(10).unwrap();
+                            }
+                            for jj in (index + 5 + num_size_1)..(index + 5 + num_size_1 + num_size_2){
+                                num2 = num2 * 10 + data[jj].to_digit(10).unwrap();
+                            }
+                            ret += (num1 * num2) as i32;
+                        }
+                    }  
+                }
+            }
+            else {
+                // if its less then 8 away then no more mul can happen
+                break;
+            }
+        }
+        // next index
+        index += 1;
+    }
+    return ret;
+}
