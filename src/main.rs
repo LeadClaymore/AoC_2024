ad1c3d9d1aae73908636a105e993a79276d3c668
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-03 09:37:56 -0500

    day 3 p1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod one;
 mod two;
+mod three;
 
 fn main() {
     //println!("{:?}", one::_one_p1());
     //println!("{:?}", one::_one_p2());
-    //println!("{:?}", two::two_p1());
-    println!("{:?}", two::two_p2());
+    //println!("{:?}", two::_two_p1());
+    //println!("{:?}", two::_two_p2());
+    println!("{:?}", three::_three_p1());
 }
