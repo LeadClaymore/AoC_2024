45cccf32fdc4b2233e31b984a5ea8f1d1245a65f
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-09 19:15:41 -0500

    I made something that finds the last block of numbers and swaps it with the first large enough free block. I need to make something that finds the first free block and finds the last largest block to swap with it

--- src/nine.rs
+++ src/nine.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+//use std::intrinsics::mir::BasicBlock;
 use std::io::{self, BufRead};
 
 
@@ -8,7 +9,7 @@ pub fn nine() -> io::Result<()> {
     let mut data = Vec::new();
     let mut answer = 0;
 
-    match read_data(String::from("data/nine.txt"), &mut data) {
+    match read_data(String::from("data/nine_test.txt"), &mut data) {
         Ok(_) =>  println!("Data read"),
         Err(ret) => return Err(ret),
     }
@@ -28,7 +29,8 @@ pub fn nine() -> io::Result<()> {
     let mut blocks = Vec::new();
 
     build_blocks(&new_data, &mut blocks);
-    compress_blocks(&mut blocks);
+    //compress_blocks(&mut blocks);
+    compress_blocks_two(&mut blocks);
     
     Ok(())
 }
@@ -120,5 +122,116 @@ fn find_answer(blocks: &Vec<Option<u32>>) {
     println!("answer = {sum}");
 }
 
+fn compress_blocks_two(blocks: &mut Vec<Option<u32>>) {
+    let mut f_block = find_last_full_block(&blocks);
+    let mut e_block = None;
+    if f_block.is_some() {
+        e_block = find_empty_space(&blocks, f_block.unwrap().1);
+    }
 
+    while 
+        e_block.is_some() && f_block.is_some() &&
+        e_block.unwrap() < f_block.unwrap().0
+    {
+        print_blocks(blocks);
+        //todo r swap
+        recursive_swap(blocks, e_block.unwrap(), e_block.unwrap() + f_block.unwrap().1 - 1, f_block.unwrap().0);
+        f_block = find_last_full_block(&blocks);
+        if f_block.is_some() {
+            e_block = find_empty_space(&blocks, f_block.unwrap().1);
+        }
+    }
+
+    find_answer(blocks);
+}
+
+fn find_empty_space(blocks: &Vec<Option<u32>>, size: usize) -> Option<usize> {
+    // this gets an iterator of the indexs of blocks that are none
+    // enumerate seperates it into an iterator of a collection of (index, value)
+    // filter in this case filters for if the value is none
+    // map takes those filtered (index, value) of only none values, and changes it only to the indexes
+
+    // aka we went from an iterator of an collection of Option then
+    // changed it to an iterator of a collection of (index, Option) where the index is where the Option was in the previous collection
+    // then we filtered for Nones in the options (aka removing all the options wtih some in them)
+    // then we reduced the (index, option) to just and index in the same order that the (index, option) collection had
+    // now we have a iterator of usize that is the indexes of Nones that were in the original collection (also in the same order)
+    for ii in blocks.iter()
+        .enumerate()
+        .filter(|(_, &v)| v.is_none())
+        .map(|(i, _)| i) 
+    {
+        // ii here is indexs of none within blocks
+
+        // iterator magic of skipping to the index we found before, 
+        // taking a slice of the size we need to check for being empty
+        // filtering by what has some in it (aka what we dont want)
+        // then getting the length of that collection of some
+        // and if its 0 (aka there is no some) then this is the index we want to return
+        if blocks.iter().skip(ii).take(size).filter(|&jj| jj.is_some()).count() == 0 {
+            return Some(ii)
+        }
+    }
+    
+    // if we cant find a spot return none, but tbh there should allways be something, if the data is entered properly
+    return None;
+}
+
+/// returns (last index, how many index)
+fn find_last_full_block(blocks: &Vec<Option<u32>>) -> Option<(usize, usize)> {
+    let mut num_to_find = None;
+    let mut counter = 0;
+    let mut last_inx = None;
+    for ii in blocks.iter().rev()
+        .enumerate().filter(|(_inx, &val)| val.is_some())
+        .map(|(inx,  _val)| inx)
+    {
+        let rev_inx = blocks.len() - 1 - ii;
+        if num_to_find.is_none() {
+            num_to_find = Some(blocks[ii]);
+            last_inx = Some(rev_inx);
+            counter += 1;
+            continue;
+        } else {
+            if num_to_find.unwrap() == blocks[ii] && rev_inx.abs_diff(last_inx.unwrap()) <= 1 {
+                counter += 1;
+                last_inx = Some(rev_inx);
+                continue;
+            }
+        }
+        break;
+    }
+
+    if last_inx != None {
+        return Some((last_inx.unwrap(), counter));
+    } else {
+        return None;
+    }
+}
+
+fn recursive_swap(blocks: &mut Vec<Option<u32>>, s_inx: usize, e_inx: usize, to_inx: usize) {
+    let dif = e_inx - s_inx;
+    if 
+        e_inx < blocks.len() &&
+        to_inx + dif < blocks.len()
+    {
+        for ii in 0..=dif {
+            blocks.swap(s_inx + ii, to_inx + ii);
+        }
+    } else {
+        println!("error trying to swap s: {s_inx}, e: {e_inx}, to: {to_inx}")
+    }
+}
+
+
+fn print_blocks(blocks: &Vec<Option<u32>>) {
+    blocks.iter().for_each(|c| {
+        if c.is_some() {
+            print!("{}", c.unwrap());
+        } else {
+            print!(".");
+        }
+    });
+    println!("");
+}
 //end
