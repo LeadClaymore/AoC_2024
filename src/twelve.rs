ce2ba65e111ad78788dcadb32062374286f36131
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-12 01:58:30 -0500

    12.1, current iteration goes off of an refrence so changes done elsewhere do not happen, I had the same problem in the trail problem, ill have to return positions of each to not dupe or get better at multithreaded opperation so I could use an constant update rather then a ref

--- src/twelve.rs
+++ src/twelve.rs
@@ -10,15 +10,14 @@ pub fn twelve() -> io::Result<()> {
         Err(ret) => return Err(ret),
     }
 
-    for line in data {
-        for ii in line {
+    for line in data.iter() {
+        for ii in line.iter() {
             print!("{ii}");
         }
         println!("");
     }
-    
-    //TODO
-    //println!("answer = {}", );
+
+    calc_data(&data);
     Ok(())
 }
 
@@ -41,4 +40,103 @@ fn read_data(file: String, data: &mut Vec<Vec<char>>) -> io::Result<()> {
     Ok(())
 }
 
+#[allow(dead_code, unused_assignments)]
+fn calc_data(data: &Vec<Vec<char>>) {
+    let mut used_data = data.clone();
+    let mut total_area = 0;
+    let mut total_prim = 0;
+    let mut total_price = 0;
+    for ii in 0..data.len() {
+        for jj in 0..data.len() {
+            // this 
+            if used_data[ii][jj] != '.' {
+                let temp_c = used_data[ii][jj];
+                if let Some((area, prim)) = calc_plot(&mut used_data, (ii, jj), temp_c) {
+                    println!("plot {ii}, {jj} has area: {area}, {prim} as {temp_c}");
+                    total_area += area;
+                    total_prim += prim;
+                    total_price += area * prim;
+                } else {
+                    println!("error at ({}, {}) within {}, found '-' ", ii, jj, used_data[ii][jj]);
+                }
+            }
+        }
+    }
+    println!("total prim {total_prim}, total area {total_area}, with the price of {total_price}");
+}
+
+// current iteration goes off of an refrence so changes done elsewhere do not happen,
+// I had the same problem in the trail problem, ill have to return positions of each to not dupe
+// or get better at multithreaded opperation so I could use an constant update rather then a ref
+
+/// returns (area, perimeter)
+#[allow(dead_code, unused_assignments)]
+fn calc_plot(used_data: &mut Vec<Vec<char>>, p: (usize, usize), c: char) -> Option<(u32, u32)> {
+    if used_data[p.0][p.1] == '-' {
+        //println!("error at ({}, {}) within {}, found '-' ", p.0, p.1, c);
+        return None;
+    }
+
+    // this means its a edge
+    if used_data[p.0][p.1] != c {
+        print!("({},{}) ", p.0, p.1);
+        return Some((0, 1));
+    }
+
+    //set this to in use
+    used_data[p.0][p.1] = '-';
+    let mut r_a = 0;
+    let mut r_p = 0;
+
+    // up
+    if p.0 > 0 {
+        // this if let would have been part of the last if but its not available
+        if let Some((t_a, t_p)) = calc_plot(used_data, (p.0 - 1, p.1), c) {
+            r_a += t_a;
+            r_p += t_p;
+        }
+    } else {
+        r_a += 0;
+        r_p += 1;
+    }
+    
+    // down
+    if p.0 < used_data.len() - 1 {
+        if let Some((t_a, t_p)) = calc_plot(used_data, (p.0 + 1, p.1), c) {
+            r_a += t_a;
+            r_p += t_p;
+        }
+    } else {
+        r_a += 0;
+        r_p += 1;
+    }
+    
+    // left
+    if p.1 > 0 {
+        if let Some((t_a, t_p)) = calc_plot(used_data, (p.0, p.1 - 1), c) {
+            r_a += t_a;
+            r_p += t_p;
+        }
+    } else {
+        r_a += 0;
+        r_p += 1;
+    }
+
+    // right
+    if p.1 < used_data[0].len() - 1 {
+        if let Some((t_a, t_p)) = calc_plot(used_data, (p.0, p.1 + 1), c) {
+            r_a += t_a;
+            r_p += t_p;
+        }
+    } else {
+        r_a += 0;
+        r_p += 1;
+    }
+
+    //after all the sides are done, set this to used
+    used_data[p.0][p.1] = '.';
+
+    // +1 because it is it self
+    return Some((r_a + 1, r_p));
+}
 //end
