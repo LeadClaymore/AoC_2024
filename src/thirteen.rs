48bf62d803a837fb6da88f70d65e50eed0a7143d
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-16 10:34:41 -0500

    13 another try does not work. 91073735289453 is too low

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -1,8 +1,13 @@
+// #[macro_use]
+// extern crate ndarray;
+// extern crate ndarray_linalg;
+
+use ndarray::prelude::*;
+//use ndarray_linalg::Solve;
+//use nalgebra::{Matrix2, Vector2};
+
 use std::fs::File;
 use std::io::{self, BufRead};
-//use ndarray::Array2;
-use ndarray::prelude::*;
-use ndarray_linalg::*;
 //use serde_scan;
 
 #[allow(dead_code, unused_assignments)]
@@ -20,7 +25,7 @@ pub fn thirteen() -> io::Result<()> {
         //println!("[{}, {}] + [{}, {}] = [{}, {}]: ", a[[0, 0]], a[[1, 0]], b[[0, 0]], b[[1, 0]], c[[0, 0]], c[[1, 0]]);
         let low = find_lowest(&a, &b, &c);
         println!("({}, {})", low.0, low.1);
-        if low != (i64::MAX, i64::MAX) && low.0 >= 0 && low.1 >= 0 {
+        if low != (i128::MAX, i128::MAX) && low.0 >= 0 && low.1 >= 0 {
             tokens += 3 * low.0 + low.1;
         }
     }
@@ -47,9 +52,10 @@ fn read_data(file: String, data: &mut Vec<Vec<char>>) -> io::Result<()> {
 }
 
 #[allow(dead_code, unused_assignments)]
-fn read_data_2(file: String) -> io::Result<Vec<(Array2<i64>, Array2<i64>, Array2<i64>)>> {
+fn read_data_2(file: String) -> io::Result<Vec<(Array2<i128>, Array2<i128>, Array2<i128>)>> {
     let mut ret = Vec::new();
     let reader = io::BufReader::new(File::open(file)?);
+    let offset = 10000000000000;
 
     let mut inx = 0;
     let shape = (2, 1);
@@ -63,9 +69,9 @@ fn read_data_2(file: String) -> io::Result<Vec<(Array2<i64>, Array2<i64>, Array2
             (a, b, c) = (Array2::zeros(shape), Array2::zeros(shape), Array2::zeros(shape));
             inx = 0;
         } else {
-            let mut v: [i64; 2] = [0, 0];
+            let mut v: [i128; 2] = [0, 0];
             let mut f_num_pushed = false;
-            let mut t_num: i64 = 0;
+            let mut t_num: i128 = 0;
             for ii in line.chars() {
                 // all this does is look for numbers, 
                 // if its a number, add it to a total, 
@@ -74,25 +80,17 @@ fn read_data_2(file: String) -> io::Result<Vec<(Array2<i64>, Array2<i64>, Array2
                 // basicly it takes any string of chars, pulls 2 numbers out of it and puts them in v
 
                 if let Some(num) = ii.to_digit(10) {
-                    t_num = t_num * 10 + num as i64;
+                    t_num = t_num * 10 + num as i128;
                     //println!("was dig {num}");
                 } else {
                     if t_num != 0 && !f_num_pushed {
-                        if inx == 2 {
-                            //for p2
-                            //t_num += 10000000000000;
-                        }
-                        v[0] = t_num as i64;
+                        v[0] = t_num as i128;
                         f_num_pushed = true;
                         t_num = 0;
                     }
                 }
             }
-            if inx == 2 {
-                // for p2
-                //t_num += 10000000000000;
-            }
-            v[1] = t_num as i64;
+            v[1] = t_num as i128;
 
             //println!("[{}][{}]", v[0], v[1]);
             if inx == 0 {
@@ -100,6 +98,8 @@ fn read_data_2(file: String) -> io::Result<Vec<(Array2<i64>, Array2<i64>, Array2
             } else if inx == 1 {
                 b = Array2::from_shape_vec(shape, v.to_vec()).unwrap();
             } else if inx == 2 {
+                v[0] += offset;
+                v[1] += offset;
                 c = Array2::from_shape_vec(shape, v.to_vec()).unwrap();
             }
             inx += 1;
@@ -113,10 +113,10 @@ fn read_data_2(file: String) -> io::Result<Vec<(Array2<i64>, Array2<i64>, Array2
 // where it does not take into account that a presses are more expensive then b presses, 
 // and my current linear algebrea does not take that into account
 #[allow(dead_code, unused_assignments, unused_variables)]
-fn find_lowest(a: &Array2<i64>, b: &Array2<i64>, c: &Array2<i64>) -> (i64, i64) {
+fn find_lowest(a: &Array2<i128>, b: &Array2<i128>, c: &Array2<i128>) -> (i128, i128) {
     let ao = 3;
     let bo = 1;
-    let co: i64 = 0; //10000000000000;
+    let co: i128 = 0; //10000000000000;
     let det = a[[0, 0]] * b[[1, 0]] - a[[1, 0]] * b[[0, 0]];
     if det != 0 {
         let ret = (
@@ -127,7 +127,7 @@ fn find_lowest(a: &Array2<i64>, b: &Array2<i64>, c: &Array2<i64>) -> (i64, i64) 
             return ret;
         }
     }
-    return (i64::MAX, i64::MAX);
+    return (i128::MAX, i128::MAX);
 
     // // find the minimum ittetations of the lowest number to reach the total
     // let mi = if a[[0, 0]] > b[[0, 0]] {
@@ -142,12 +142,12 @@ fn find_lowest(a: &Array2<i64>, b: &Array2<i64>, c: &Array2<i64>) -> (i64, i64) 
     // };
 
     // //print!("{mi} {mj}: ");
-    // let mut ret = (i64::MAX, i64::MAX);
+    // let mut ret = (i128::MAX, i128::MAX);
     // for ii in 0..=mi {
     //     for jj in 0..=mj {
     //         if a * ii + b * jj == c {
     //             //print!("[{ii}][{jj}], ");
-    //             if ret.0 == i64::MAX || a_but * ii + b_but * jj < ret.0 + ret.1 {
+    //             if ret.0 == i128::MAX || a_but * ii + b_but * jj < ret.0 + ret.1 {
     //                 ret.0 = ii;
     //                 ret.1 = jj;
     //             }
@@ -158,13 +158,28 @@ fn find_lowest(a: &Array2<i64>, b: &Array2<i64>, c: &Array2<i64>) -> (i64, i64) 
     // return ret;
 }
 
-fn find_lowest_2(a: &Array2<i64>, b: &Array2<i64>, c: &Array2<i64>) -> (i64, i64) {
-    let a_matrix: Array2<i64> = array![[a[[0, 0]], b[[0, 0]]],[a[[1, 0]], b[[1, 0]]]];
-    let l_vector: Array1<i64> = array![c[[0, 0]], c[[1, 0]]];
-    // solve is meant to work
-    // I even found the docs.rs on it
-    // https://docs.rs/ndarray-linalg/latest/ndarray_linalg/solve/
-    let solution = a_matrix.solve_into(l_vector).unwrap();
-    return (0,0);
-}
+// #[allow(dead_code, unused_assignments, unused_variables)]
+// fn find_lowest_2(a: &Array2<i128>, b: &Array2<i128>, c: &Array2<i128>) -> (i128, i128) {
+//     // let aa: nalgebra::Matrix<i128, nalgebra::Const<2>, nalgebra::Const<2>, nalgebra::ArrayStorage<i128, 2, 2>> = Matrix2::new(a[[0, 0]], b[[0, 0]], a[[1, 0]], b[[1, 0]]);
+//     // let bb = Vector2::new(c[[0, 0]], c[[1, 0]]);
+
+//     // match a.try_inverse() {
+//     //     Some(inv_a) => {
+//     //         let solution = inv_a * b;
+//     //         println!("Unique solution: {:?}", solution);
+//     //     },
+//     //     None => {
+//     //         return (0, 0);
+//     //     }
+//     // }
+
+
+//     // let aa: Array2<i128> = array![[a[[0, 0]], b[[0, 0]]],[a[[1, 0]], b[[1, 0]]]];
+//     // let vv: Array2<i128> = array![c[[0, 0]], c[[1, 0]]];
+//     // solve is meant to work
+//     // I even found the docs.rs on it
+//     // https://docs.rs/ndarray-linalg/latest/ndarray_linalg/solve/
+//     // let solution = aa.solve_into(vv).unwrap();
+//     return (0,0);
+// }
 //end
