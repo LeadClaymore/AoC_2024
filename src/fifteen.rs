39974a3dd68b7718bdabaad6730079e2a7e9c086
LeadClaymore <ClaytonGormanRoss@gmail.com>
2024-12-18 02:52:25 -0500

    not done bug feeling good about 15

--- src/fifteen.rs
+++ src/fifteen.rs
@@ -34,7 +34,7 @@ impl Dir {
 
 #[allow(dead_code, unused_assignments)]
 pub fn fifteen() -> io::Result<()> {
-    let (mut maze, moves, s_pos) = match read_data_2(String::from("data/15/data.txt")) {
+    let (mut maze, moves, s_pos) = match read_data_2(String::from("data/15/test.txt")) {
         Ok(stuff) =>  {
             println!("Data read");
             stuff
@@ -44,6 +44,7 @@ pub fn fifteen() -> io::Result<()> {
     
     let mut pos = s_pos.clone();
     print_maze(&maze);
+    //p1
     //need to take the position its moving to and check if there is 
     //1 imoveable blocks,
     //2 moveable blocks with clear spaces to move to (contiue checking in the same way)
@@ -67,7 +68,10 @@ pub fn fifteen() -> io::Result<()> {
     let mut total = 0;
     for ii in 0..maze.len() {
         for jj in 0..maze.len() {
-            if maze[ii][jj] == 'O' {
+            if 
+                maze[ii][jj] == 'O' ||
+                maze[ii][jj] == '[' 
+            {
                 total += 100 * ii + jj;
             }
         }
@@ -109,9 +113,22 @@ fn read_data_2(file: String) -> io::Result<(Vec<Vec<char>>, Vec<Dir>, (usize, us
             let mut line_jj = 0;
             for c in line.chars() {
                 if c == '@' {
-                    s_pos = (line_inx, line_jj);
+                    s_pos = (line_inx, line_jj * 2);
+                    //s_pos = (line_inx, line_jj);
+                    line_c.push('@');
+                    line_c.push('.');
+                } else if c == '#' {
+                    line_c.push('#');
+                    line_c.push('#');
+                } else if c == 'O' {
+                    line_c.push('[');
+                    line_c.push(']');
+                } else if c == '.' {
+                    line_c.push('.');
+                   line_c.push('.');
+                } else {
+                    println!("Error on {line_inx}, {line_jj}");
                 }
-                line_c.push(c);
                 line_jj += 1;
             }
 
@@ -136,10 +153,15 @@ fn in_bound(ii: usize, jj: usize, mi: usize, mj: usize, dif: (usize, usize)) -> 
     }
 }
 
+// this works for p2 when horizontal, so ima use it
 #[allow(dead_code, unused_assignments)]
 fn try_to_move(mut maze: Vec<Vec<char>>, ii: usize, jj: usize, dd: Dir) -> (bool, Vec<Vec<char>>) {
     if let Some((ni, nj)) = in_bound(ii, jj, maze.len(), maze[0].len(), dd.c_cords()) {
-        if maze[ni][nj] == 'O' {
+        if 
+            maze[ni][nj] == 'O' || 
+            maze[ni][nj] == '[' || 
+            maze[ni][nj] == ']'
+        {
             let (ret, mut maze) = try_to_move(maze, ni, nj, dd);
             if ret == true {
                 maze[ni][nj] = maze[ii][jj];
@@ -156,6 +178,56 @@ fn try_to_move(mut maze: Vec<Vec<char>>, ii: usize, jj: usize, dd: Dir) -> (bool
         }
     } else {
         return (false, maze);
+    }
+}
+
+//TODO plan
+// I dont need the in bound check so ima just scrap it
+// The only diffrence is with the moving block thing
+// horizontaly I can reuse try_to_move() so ima foucus on vertical moves
+// this will work completely different from the first iteration
+// it will create a list of moves, by branching out through each possible effect of each move
+// and if the entire move is valid, elsewhere it will apply the move 
+
+// TODO the move needs to happen opposite of the dirrection of the push so it does not delete stuff
+// aka if we push up then the effects have to happen top down
+
+/// Important: this takes pos of next move not cur move
+/// Will return maze and either 
+/// None: push is bad. 
+/// Some(Vec.empty()): edge '.' space but push is still good.
+/// Some(Vec.not_empty()) contents to be move also move is good.
+#[allow(dead_code, unused_assignments)]
+fn try_to_move_p2(maze: Vec<Vec<char>>, ii: usize, jj: usize, dd: Dir) -> (Option<Vec<(usize, usize)>>, Vec<Vec<char>>) {
+    let (nni, nnj);
+
+    // unless block we dont need to calculate anything
+    if maze[ii][jj] == '.' {
+        return (Some(Vec::new()), maze);
+    } else if maze[ii][jj] == '#' {
+        return (None, maze);
+    } else if maze[ii][jj] == '[' {
+        //find other half
+        (nni, nnj) = (ii, jj + 1);
+    } else if maze[ii][jj] == ']' {
+        //find other half
+        (nni, nnj) = (ii, jj - 1);
+    } else {
+        return (None, maze);
+    }
+    // if it makes it here then its a block, so we try to recurse with both halfs
+    let dif = dd.c_cords();
+    let (tomv_1, maze) = try_to_move_p2(maze, ii + dif.0, jj + dif.1, dd);
+    let (tomv_2, maze) = try_to_move_p2(maze, nni + dif.0, nnj + dif.1, dd);
+
+    // wild if let to get both returns
+    if let (Some(mut ret_1), Some(mut ret_2)) = (tomv_1, tomv_2) {
+        // combine and return all the changes that must be made
+        ret_1.append(&mut ret_2);
+        return (Some(ret_1), maze);
+    } else {
+        // if one of the changes wont work, none of them will
+        return (None, maze);
     }
 }
 
